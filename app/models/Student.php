c6f3849f3ebaa52141b5c164dc17a77947d76e46
rahman5147 <rahmanromli@yahoo.com>
2014-10-28 03:16:09 +0800

    change Student model rules

--- app/models/Student.php
+++ app/models/Student.php
@@ -4,6 +4,12 @@ class Student extends Eloquent implements Robbo\Presenter\PresentableInterface {
 
 	protected $guarded = array();
 	protected $table = 'student';
+	public static $rules = array(
+		'email' => 'required|min:2',
+		'password' => 'required|min:5',
+		'first_name' => 'required|min:3',
+		'last_name' => 'required|min:3',
+	);
 
 	public function getPresenter(){
 		return new StudentPresenter($this);

0897ac6fedad531dc304ee4e142cb14153cd049b
Rahman <rahmanromli@yahoo.com>
2016-09-02 20:20:20 +0800

    update student table seeder

--- database/seeds/StudentsTableSeeder.php
+++ database/seeds/StudentsTableSeeder.php
@@ -1,28 +1,23 @@
 <?php
-
-// Composer: "fzaninotto/faker": "v1.3.0"
-// use Faker\Factory as FakerFactory;
-// use Faker\Generator as Generator;
-// use Faker\Provider\Internet;
+use Illuminate\Database\Seeder;
 
 class StudentsTableSeeder extends Seeder {
 
 	public function run(){
-
-		DB::table('student')->delete();
-		// $faker = Faker::create();
-		$faker = new Faker\Generator();
-		$faker->addProvider(new Faker\Provider\en_US\Person($faker));
-		$faker->addProvider(new Faker\Provider\Internet($faker));
-		$faker->addProvider(new Faker\Provider\en_US\Company($faker));
-
-		foreach(range(1, 3) as $index){
-			Student::create([
-				'first_name' => $faker->firstName,
+		$faker = Faker\Factory::create();
+		DB::table('student')->insert([
+            'first_name' => 'Rahman',
+			'last_name' => 'K',
+			'password' => $faker->password,
+			'email' => 'rahmanromli@yahoo.com',
+        ]);
+		for ($i=0; $i < 10; $i++) {
+			DB::table('student')->insert([
+	            'first_name' => $faker->firstName,
 				'last_name' => $faker->lastName,
 				'password' => $faker->password,
 				'email' => $faker->email,
-			]);
+	        ]);
 		}
 	}
 }
